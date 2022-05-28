2a0cf8906590dbce35bda7fb3c3bc07d092b9b58
jongming <jongming@gmail.com>
2022-05-28 13:35:11 +0900

    first change

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Main(){
+    return(
+        <main>
+        <h1>Fun fact about React</h1>
+        <ul>
+            <li>abc</li>
+            <li>def</li>
+            <li>ghi</li>
+        </ul>
+    </main>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactLogo from "../images/react-icon-small.png"
+
+export default function Navbar(){
+    return(
+        <na>
+            <img src="{reactLogo}"></img>
+            <h3>ReactFacts</h3>
+            <h3>React Course: Project 1</h3>
+        </na>
+    )
+}
